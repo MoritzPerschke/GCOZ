865db3186ee60c0e6d84c941dfe24310580c1db4
Moritz Perschke <perschkemoritz@gmail.com>
2023-06-07 19:18:34 +0200

    injection works, game no longer crashes

--- gcoz_dll/dllmain.cpp
+++ gcoz_dll/dllmain.cpp
@@ -4,16 +4,16 @@
 #include <Windows.h>
 #include "kiero/kiero.h"
 
-typedef long(__stdcall* Present)(IDXGISwapChain*, UINT, UINT);
+typedef HRESULT(__stdcall* Present)(IDXGISwapChain*, UINT, UINT);
 static Present oPresent = NULL;
 
-long __stdcall hkPresent11(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags)
+HRESULT __stdcall hkPresent11(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags)
 {
 	static bool init = false;
 
 	if (!init)
 	{
-        MessageBoxW(0, L"Boom", L"works :D", MB_SETFOREGROUND);
+		MessageBoxW(0, L"Boom", L"works :D", MB_SETFOREGROUND);
 		init = true;
 	}
 
@@ -22,26 +22,26 @@ long __stdcall hkPresent11(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT F
 }
 
 int kieroExampleThread() {
-    if (kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success) {
-        kiero::bind(8, (void**)&oPresent, hkPresent11);
-        oPresent = (Present)kiero::getMethodsTable()[8];
-    }
-    return 0;
+	if (kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success) {
+		kiero::bind(8, (void**)&oPresent, hkPresent11);
+	}
+	return 0;
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
-    DisableThreadLibraryCalls(hModule);
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)kieroExampleThread, NULL, 0, NULL);
-        //MessageBox(NULL, L"MainBox", L"dll", MB_SETFOREGROUND); // This one shows, doesn't pop up, just opened in bg
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	DisableThreadLibraryCalls(hModule);
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)kieroExampleThread, NULL, 0, NULL);
+		break;
+
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
 }
 
