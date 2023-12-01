97508b2f24104dfb029b96bc234eaffc8dfa862c
Moritz Perschke <perschkemoritz@gmail.com>
2023-12-01 13:31:49 +0100

    seperate method to write data from dll to shared mem

--- gcoz_dll/MethodDurations.cpp
+++ gcoz_dll/MethodDurations.cpp
@@ -6,11 +6,13 @@ Timepoint MethodDurations::now() {
 
 void MethodDurations::addDuration(int _methodIdx, RawDuration _dur) {
 
-	auto it = _durations->find(_methodIdx);
-	if (it == _durations->end()) {
-		it = _durations->emplace(_methodIdx, _durations->get_allocator()).first;
+	if(duration_cast<Nanoseconds>(_dur).count() > 0){
+		auto it = _durations->find(_methodIdx);
+		if (it == _durations->end()) {
+			it = _durations->emplace(_methodIdx, _durations->get_allocator()).first;
+		}
+		it->second.push_back(duration_cast<Nanoseconds>(_dur));
 	}
-	it->second.push_back(duration_cast<Nanoseconds>(_dur));
 }
 
 void MethodDurations::presentStart(int _method, int _delay, int &_counter) {
@@ -21,36 +23,39 @@ void MethodDurations::presentStart(int _method, int _delay, int &_counter) {
 	}
 
 	else if(_counter < MEASURE_FRAME_COUNT) {
-		/* get iterators to the map for the method and for the map for the delay */
-		auto rates_method_it = _frameRates->find(_method);
-		if (rates_method_it == _frameRates->end()) {
-			rates_method_it = _frameRates->emplace(_method, _frameRates->get_allocator()).first; // insert map for method results and assign iterator to new map
-		}
-		
-		auto rates_delay_it = rates_method_it->second.find(_delay);
-		if (rates_delay_it == rates_method_it->second.end()) {
-			rates_delay_it = rates_method_it->second.emplace(_delay, rates_method_it->second.get_allocator()).first;
-		}
-
-		Nanoseconds rate = duration_cast<Nanoseconds> (currentPresentStart - lastPresentStart);
-		named_mutex ratesMutex(open_only, "gcoz_Framerates_Map_Mutex");
-		scoped_lock<named_mutex> ratesLock(ratesMutex);
-		rates_delay_it->second.push_back(rate);
-
-		auto times_method_it = _frameTimes->find(_method);
-		if (times_method_it == _frameTimes->end()) {
-			times_method_it = _frameTimes->emplace(_method, _frameTimes->get_allocator()).first;
-		}
-
-		auto times_delay_it = times_method_it->second.find(_delay);
-		if (times_delay_it == times_method_it->second.end()) {
-			times_delay_it = times_method_it->second.emplace(_delay, times_method_it->second.get_allocator()).first;
-		}
-
-		Nanoseconds time = duration_cast<Nanoseconds> (currentPresentStart - lastPresentEnd);
 		named_mutex timesMutex(open_only, "gcoz_FrameTimes_Map_Mutex");
 		scoped_lock<named_mutex> timesLock(timesMutex);
-		times_delay_it->second.push_back(time);
+		frameTimes[_method][_delay].push_back(duration_cast<Nanoseconds> (currentPresentStart - lastPresentEnd));
+		///* get iterators to the map for the method and for the map for the delay */
+		//auto rates_method_it = _frameRates->find(_method);
+		//if (rates_method_it == _frameRates->end()) {
+		//	rates_method_it = _frameRates->emplace(_method, _frameRates->get_allocator()).first; // insert map for method results and assign iterator to new map
+		//}
+		//
+		//auto rates_delay_it = rates_method_it->second.find(_delay);
+		//if (rates_delay_it == rates_method_it->second.end()) {
+		//	rates_delay_it = rates_method_it->second.emplace(_delay, rates_method_it->second.get_allocator()).first;
+		//}
+
+		//Nanoseconds rate = duration_cast<Nanoseconds> (currentPresentStart - lastPresentStart);
+		//named_mutex ratesMutex(open_only, "gcoz_Framerates_Map_Mutex");
+		//scoped_lock<named_mutex> ratesLock(ratesMutex);
+		//rates_delay_it->second.push_back(rate);
+
+		//auto times_method_it = _frameTimes->find(_method);
+		//if (times_method_it == _frameTimes->end()) {
+		//	times_method_it = _frameTimes->emplace(_method, _frameTimes->get_allocator()).first;
+		//}
+
+		//auto times_delay_it = times_method_it->second.find(_delay);
+		//if (times_delay_it == times_method_it->second.end()) {
+		//	times_delay_it = times_method_it->second.emplace(_delay, times_method_it->second.get_allocator()).first;
+		//}
+
+		//Nanoseconds time = duration_cast<Nanoseconds> (currentPresentStart - lastPresentEnd);
+		//named_mutex timesMutex(open_only, "gcoz_FrameTimes_Map_Mutex");
+		//scoped_lock<named_mutex> timesLock(timesMutex);
+		//times_delay_it->second.push_back(time);
 	}
 	lastPresentStart = currentPresentStart;
 }
@@ -62,4 +67,26 @@ void MethodDurations::presentEnd() {
 
 void MethodDurations::reset() {
 	presentCalledInit = false;
+}
+
+void MethodDurations::finish(int _method, int _delay) {
+	named_mutex timesMutex(open_only, "gcoz_FrameTimes_Map_Mutex");
+	scoped_lock<named_mutex> timesLock(timesMutex);
+
+	auto method_it = _frameTimes->emplace(_method, _frameTimes->get_allocator()).first;
+	auto delay_it = method_it->second.emplace(_delay, method_it->second.get_allocator()).first;
+
+	for (const auto& time : frameTimes[_method][_delay]) {
+		delay_it->second.push_back(time);
+	}
+
+	//DisplayInfoBox(L"Length of map<<>>", std::to_wstring(_frameTimes->size()));
+	//for (const auto& method : frameTimes) {
+	//	auto method_it = _frameTimes->emplace(method.first, _frameTimes->get_allocator()).first;
+	//	DisplayInfoBox(L"Length of map<" + std::to_wstring(method.first) + L", vec>: ", std::to_wstring(frameTimes[method.first].size()));
+	//	for (const auto& delay : method.second) {
+	//		auto delay_it = method_it->second.emplace(delay.first, method_it->second.get_allocator()).first;
+	//		DisplayInfoBox(L"Length of vec: ", std::to_wstring(frameTimes[method.first][delay.first].size()));
+	//	}
+	//}
 }
--- gcoz_dll/MethodDurations.h
+++ gcoz_dll/MethodDurations.h
@@ -8,13 +8,23 @@
 
 #include "ErrorMessage.h"
 
+
+#include <map>
+#include <vector>
+
 class MethodDurations {
 	bool presentCalledInit = false;
 	Timepoint lastPresentStart;
 	Timepoint lastPresentEnd;
 
 	static std::chrono::steady_clock clock;
-	
+
+	//using DurationVector_Map = Map<int, Vector<Nanoseconds>>; // Method Durations Map (int -> Vector<Duration>)
+	//using ThreadIdVector_Map = Map<int, Vector<idHash>>; //Map for ThreadIDs (int -> Vector<idHash>)
+	//using ResultsMap_Map = Map<int, Map<int, Vector<Nanoseconds>>>; // Results Map (int -> float -> Vector<Duration>) // does not work because allocator tries to alloc pair <int, Map<>> for pair <float, vec<llong>>
+
+	std::map<int, std::map<int, std::vector<Nanoseconds>>> frameTimes;
+
 	managed_shared_memory segment;
 	IPC::DurationVector_Map* _durations;
 	IPC::ResultsMap_Map* _frameTimes;
@@ -30,6 +40,7 @@ public:
 	Timepoint now();
 	void addDuration(int _methodIdx, RawDuration _duration);
 	void presentStart(int _method, int _delay, int &_counter);
+	void finish(int _method, int _delay);
 	void presentEnd();
 	void reset();
 };
